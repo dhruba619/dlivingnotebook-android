4058727bf76ad6b07a38621c19486789facbc3c1
A676491 <dhrubajyoti.bhattacharjee>
2018-07-06 16:26:37 +0530

    Started building the profile section

--- app/src/main/java/in/dlivingnotebook/dlivingnotebook/Profile/ProfileActivity.java
+++ app/src/main/java/in/dlivingnotebook/dlivingnotebook/Profile/ProfileActivity.java
@@ -3,6 +3,7 @@ package in.dlivingnotebook.dlivingnotebook.Profile;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,9 +20,30 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: created");
         setupBottomNavigationView();
+        setupToolBar();
+    }
+
+    private void setupToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
+
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: going to profile settings");
+                        break;
+                }
+                
+                return false;
+            }
+        });
     }
 
     /**
@@ -36,5 +58,11 @@ public class ProfileActivity extends AppCompatActivity {
         MenuItem item = menu.getItem(ACTIVITY_NO);
         item.setChecked(true);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
